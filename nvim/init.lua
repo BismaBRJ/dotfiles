a017979f7b8cf94848df0ab1d42b6cbd9fe7c8bc
BismaBRJ <bismabrj@gmail.com>
2025-12-23 17:38:55 +0700

    Added neovim init.lua

---
+++ nvim/init.lua
@@ -0,0 +1,22 @@
+local opt = vim.opt
+
+-- from:
+-- https://neovim.io/doc/user/usr_30.html
+
+-- :set shiftwidth=4
+opt.shiftwidth = 4
+
+-- :set softtabstop=-1
+-- to use the default as provided by shiftwidth
+opt.softtabstop = -1
+
+-- :set expandtab
+-- to expand the tab character as spaces
+opt.expandtab = true
+
+-- more:
+
+-- absolute line numbers
+opt.number = true
+-- relative: opt.relativenumber
+
