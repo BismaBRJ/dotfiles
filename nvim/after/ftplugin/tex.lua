0a819ffa86bf44244c3f8878bb6e63920ba424de
BismaBRJ <bismabrj@gmail.com>
2025-12-30 14:26:21 +0700

    Made word wrap follow indentation by visually indenting, for .tex files

--- nvim/after/ftplugin/tex.lua
+++ nvim/after/ftplugin/tex.lua
@@ -4,3 +4,8 @@ local opt = vim.opt_local
 opt.linebreak = true
 -- as opposed to "wrap" which can split mid-word
 
+-- so that word wrap follows indentation as well,
+-- that is by visually indenting,
+-- just as word wrap is visual wrapping
+opt.breakindent = true
+
